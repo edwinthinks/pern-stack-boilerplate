487a09e73170f56708e096988347eb8550d256c8
Edwin Mak <edwinthinks@gmail.com>
2020-02-02 20:17:35 -0500

    Enable .env reading for server

--- server/index.js
+++ server/index.js
@@ -2,13 +2,30 @@ import logger from 'loglevel'
 import startServer from './src/start.js'
 
 /*
- * Set the corresponding log level to show more or less logs
- * depending on the NODE_ENV. Production is set to show less
- * logs whereas others are set to show more.
+ * Store a boolean value that indicates if the
  */
 const isProd = process.env.NODE_ENV === 'production'
+
+/*
+ * Set the corresponding log level to show more or less logs
+ * depending if in production or a env variable defines it.
+ * Production normallys shows less logs whereas others are
+ * set to show more.
+ */
 const logLevel = process.env.LOG_LEVEL || (isProd ? 'warn' : 'info')
 logger.setLevel(logLevel)
 
+/*
+ * Load environment variables for non production through
+ * the .env file located in `/server`
+ */
+if (!isProd) {
+  require('dotenv').config()
+}
+
+/*
+ * Start the server!
+ */
 startServer()
+
 
