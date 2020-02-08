02a42070ffaf0a80cc2e2ba3ff8c7b98351c01b2
Edwin Mak <edwinthinks@gmail.com>
2020-02-08 09:07:01 -0500

    Add sample fetch data in client from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // **SAMPLE**
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetchUsers();
+  },[])
+
+  async function fetchUsers() {
+    const response = await fetch('/api/users');
+    const json = await response.json();
+
+    setUsers(json);
+  };
+  // **SAMPLE**
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +33,12 @@ function App() {
         >
           Learn React
         </a>
+          {users.length > 0 &&
+            <div>
+              <h1>👇 Fetched Server Data 👇</h1>
+              <code> {JSON.stringify(users)} </code>
+            </div>
+          }
       </header>
     </div>
   );
